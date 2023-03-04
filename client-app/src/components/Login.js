b2ce07970c30079e4fbe00495ba377348215dfa7
dancunrandy <dancunrandy@gmail.com>
2023-03-04 14:41:34 +0300

    login details

---
+++ client-app/src/components/Login.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const handleLogin = (e) => {
+    e.preventDefault();
+    fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    })
+    .then(response => {
+      if (response.ok) {
+        // Login successful, redirect to home page or perform other actions
+        console.log('Login successful!');
+      } else {
+        // Login failed, display error message or perform other actions
+        console.log('Login failed!');
+      }
+    })
+    .catch(error => {
+      // Handle error
+      console.error('Error logging in:', error);
+    });
+  }
+  
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
